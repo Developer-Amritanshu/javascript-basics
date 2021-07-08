044e75edd9859659982339385d08b4c56bbdd05f
Developer-Amritanshu <amritanshu360@gmail.com>
2021-07-08 22:43:33 +0530

    04 - first commit

---
+++ 04_bool.js
@@ -0,0 +1,10 @@
+// 04 - Undefined, Null, Boolean
+
+// null and undefined is not 0
+
+// boolean - true/false
+
+// grade system - 10 (Great Job), 5 (Work hard)
+let marks = 7
+let grade = (marks < 10) 
+console.log(grade)
