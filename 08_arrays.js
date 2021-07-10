202b3fe0ee025fd0c4da79d2024ee62534640ccb
Developer-Amritanshu <amritanshu360@gmail.com>
2021-07-10 19:56:07 +0530

    08 - first commit

---
+++ 08_arrays.js
@@ -0,0 +1,8 @@
+// 08 - arrays and templates
+
+const superHeroes = ['Iron Man','Spider Man'] // we can have multiple datatype in array
+
+console.log(superHeroes)
+console.log(superHeroes[0])
+console.log(superHeroes.length) // returns length of array
+console.log(`Length : ${superHeroes.length}`) // String template ${}
